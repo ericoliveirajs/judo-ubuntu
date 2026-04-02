756cc6f2b28b57865076420d60d7c7c2df51ecf4
Eric Oliveira <ericoliveirajs@gmail.com>
2026-04-02 15:22:57 -0300

    Correções

--- apps/frontend/src/app/app.config.ts
+++ apps/frontend/src/app/app.config.ts
@@ -1,9 +1,5 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-} from '@angular/core';
-// Importação corrigida aqui:
-import { provideRouter, withInMemoryScrolling } from '@angular/router'; 
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { appRoutes } from './app.routes';
 import {
   provideClientHydration,
@@ -13,13 +9,11 @@ import {
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay()),
-    provideBrowserGlobalErrorListeners(),
     provideRouter(
       appRoutes,
-      // Aqui as duas configurações que a gente queria agora vivem juntas:
-      withInMemoryScrolling({ 
-        scrollPositionRestoration: 'enabled', 
-        anchorScrolling: 'enabled' 
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
       })
     ),
   ],
